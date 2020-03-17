05a57690bc60b1fb0d72dd0cd2b1b8f4fef2fdc3
Lindsey Cason <lindseyacason@gmail.com>
2020-03-17 13:35:52 -0500

    fix mob nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,6 +4,12 @@ import Nav from 'react-bootstrap/Nav'
 import NavDropdown from "react-bootstrap/NavDropdown"
 const Navigation=()=>{
 
+    const dropdownShow=()=>{
+        var el = document.getElementById("mobNavA")
+        console.log(el)
+        // el.classList.add("NavDropdownOff")
+    }
+
     return(
         
         
@@ -25,14 +31,23 @@ const Navigation=()=>{
                 <a href="#resume" className="navLinks navDesk resumeLink">RESUME</a>
             </div>
 
-            <NavDropdown title={<i class="fas fa-bars burg" style={{fontSize: 40, color: "white"}}/>} id="basic-nav-dropdown" className="Nav-Drop navMob">
+            {/* <NavDropdown title={<i class="fas fa-bars burg" style={{fontSize: 40, color: "white"}}/>} id="basic-nav-dropdown" className="Nav-Drop navMob">
                 <><NavDropdown.Item eventKey="4.2" href="#about" className="mobnavLinks">ABOUT</NavDropdown.Item>
                 <NavDropdown.Item eventKey="4.1" href="#myWork" className="mobnavLinks">WORK</NavDropdown.Item>
                 </>
                 <><NavDropdown.Item eventKey="4.2"href="#contact" className="mobnavLinks">CONTACT</NavDropdown.Item>
                 <NavDropdown.Item eventKey="4.3" href="#resume" className="mobnavLinks">RESUME</NavDropdown.Item></>
-            </NavDropdown>
-        </div>
+            </NavDropdown> */}
+            <div className="mobNav">
+                {/* <div className="mobNavMenu"><i className="fas fa-bars burg" style={{fontSize: 40, color: "white"}}/></div> */}
+                    
+                    <div className="mobnavLinks"> <a href="#about" className="navLinks">ABOUT</a></div>
+                    <div className="mobnavLinks"><a  href="#myWork" className="navLinks">WORK</a></div>
+                    <div className="mobnavLinks"><a href="#contact" className="navLinks">CONTACT</a></div>
+                    <div className="mobnavLinks"><a href="#resume" className="navLinks">RESUME</a></div>
+                </div>
+            </div>
+    
         
         
     )
